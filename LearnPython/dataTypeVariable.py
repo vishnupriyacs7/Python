83a10f2114d10cc4972cdce7fffd654a6b41ef53
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-29 23:36:45 +0530

    Data types and variable

---
+++ LearnPython/dataTypeVariable.py
@@ -0,0 +1,54 @@
+a = 10
+print(a)
+num = 10.5
+print(num)
+name = "Abhi"
+print(name)
+
+#naming a variable
+# 1. letter or underscore
+# 2. cannot start with number
+# 3. Alphanumeric only A-Z a-z 0-9 _
+# 4. Case sensitive Name , name not same
+
+# datatype : type(variable_name)
+print(type(a))
+print(type(num))
+print(type(name))
+
+c = 10j # j for complex number
+print(c)
+print(type(c))
+
+#boolean
+b1 = True
+b2 = False
+print(b1)
+print(b2)
+print(type(b1))
+print(type(b2))
+
+# 3 variables
+a = 10
+b = 10
+c = 10
+print(a)
+print(a)
+print(a)
+
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+
+a = "Tomato"
+b = "Onion"
+c = "Carrot"
+print(a)
+print(b)
+print(c)
+
+a,b,c = "Tomato","Carrot","Chillie"
+print(a)
+print(b)
+print(c)
