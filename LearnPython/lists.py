b1ef387945956dcb76c4d33c33629a94246c9443
vishnupriya <vishnupriyacs7@gmail.com>
2025-06-30 23:29:57 +0530

    list

---
+++ LearnPython/lists.py
@@ -0,0 +1,8 @@
+# collection datatypes - list, tuple, set, dictionary
+# 1. List
+#=============
+a = "Hello" #normal
+list1 = ["hii","hello","nice"]
+print(list1)
+print(len(list1))
+print(list1[0]) #hii
