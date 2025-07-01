f739475d15c08d06ae6e6196a194ec84c85a00ef
vishnupriya <vishnupriyacs7@gmail.com>
2025-07-01 22:18:26 +0530

    for

---
+++ LearnPython/for.py
@@ -0,0 +1,13 @@
+a = "hello"
+for i in a[2]:
+    print(i)
+#     list
+a = ["python","php","java"]
+for i in a:
+    print(i)
+
+for i in range(5): #range 0-4 total 5
+    print(i)
+
+for i in range(1,15): #range 1-14 
+    print(i)
