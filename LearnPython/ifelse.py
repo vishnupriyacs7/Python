8c2ce2213eefa348b52c1f9c34ad747e93882d7d
vishnupriya <vishnupriyacs7@gmail.com>
2025-07-01 22:10:14 +0530

    if else

---
+++ LearnPython/ifelse.py
@@ -0,0 +1,24 @@
+a = 0
+if a > 0 :
+    print("positive number")
+elif a < 0 :
+    print("Negative number")
+else :
+    print("Number is zero")
+
+# nested if
+a = 0
+if a >= 0:
+    if a > 0 :
+        print("positive num")
+    else:
+        print("zero")
+else:
+    print("negative")
+
+# logical operator
+a = 100
+if a > 5 and a < 20 :
+    print("hello")
+
+
