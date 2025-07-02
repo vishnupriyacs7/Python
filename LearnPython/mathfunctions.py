76ad3592676a6710d7bf10874f816c02fd7bb08b
vishnupriya <vishnupriyacs7@gmail.com>
2025-07-02 23:52:37 +0530

    math functions

---
+++ LearnPython/mathfunctions.py
@@ -0,0 +1,20 @@
+import math
+# minimum
+a = min(12,30,65,3,21)
+print(a)
+
+# maximum
+a = max(12,30,65,3,21)
+print(a)
+
+# power
+a = pow(2,5)
+print(a)
+
+# math module for math functions - import it
+a = math.pi
+print(a)
+a = math.sqrt(81)
+print(a)
+a = math.gcd(8,12)
+print(a)
